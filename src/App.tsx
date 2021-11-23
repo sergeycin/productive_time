dbc8f72c553b0c09cde865956c9a30b4643a55e5
sergeycin <67867645+sergeycin@users.noreply.github.com>
2021-11-23 10:01:46 +0300

    compile

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import Dark from './pages/Dark';
 import White from './pages/White';
-import { UserActionTypes, useTypesSelector } from './store/reducers/themeReducer';
+import {  useTypesSelector } from './store/reducers/themeReducer';
 
 const App: React.FC = () => {
-  const dispatch = useDispatch();
+
   const themen = useTypesSelector(state => state.theme)
   console.log(themen);
 
-  const whiteHandler = () =>{
-    dispatch({type: UserActionTypes.WHITE_THEME, payload: 1})
-  }
 
-  const darkHendler = () =>{
-    dispatch({type: UserActionTypes.DARK_THEME, payload: 0})
-  }
 
   function currentTheme() {
     if(themen == 0){
